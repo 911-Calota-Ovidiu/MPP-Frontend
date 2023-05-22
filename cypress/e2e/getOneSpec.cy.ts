2aee809ff8aea0669529ce6110a2e60d17989878
911-Calota-Ovidiu <ovidiu.calota@stud.ubbcluj.ro>
2023-05-22 17:00:00 +0300

    s

--- cypress/e2e/getOneSpec.cy.ts
+++ cypress/e2e/getOneSpec.cy.ts
@@ -2,6 +2,6 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://main--mppateveryone.netlify.app/')
     cy.get("#adultButton").should('exist').click();
-
+    cy.get("#getOne({{}}")
   })
 })
