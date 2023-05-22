8ff280ddc0f437af03f7cff1a9112ea17a78b6ab
911-Calota-Ovidiu <ovidiu.calota@stud.ubbcluj.ro>
2023-05-22 17:18:51 +0300

    ss

--- cypress/e2e/childStatistics.cy.ts
+++ cypress/e2e/childStatistics.cy.ts
@@ -4,6 +4,6 @@ describe('template spec', () => {
     cy.get("#childButton").should('exist').click();
     cy.wait(1500);
     cy.get("#childStat").should('exist').click();
-    cy.get("#h3").contains("4.9997");
+    cy.get("h3").contains("4.9997");
   })
 })
