0fbfef3785ac8bb20ff3e3085b75cc1d55415a58
911-Calota-Ovidiu <ovidiu.calota@stud.ubbcluj.ro>
2023-05-22 17:27:53 +0300

    ds

--- cypress/e2e/childStatistics.cy.ts
+++ cypress/e2e/childStatistics.cy.ts
@@ -2,7 +2,7 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://main--mppateveryone.netlify.app/');
     cy.get("#childButton").should('exist').click();
-    cy.wait(1500);
+
     cy.get("#childStat").should('exist').click();
     cy.get("h3").contains("4.9997");
   })
--- cypress/e2e/getOneSpec.cy.ts
+++ cypress/e2e/getOneSpec.cy.ts
@@ -2,7 +2,6 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://main--mppateveryone.netlify.app/')
     cy.get("#adultButton").should('exist').click();
-    cy.wait(1500);
     cy.get("#getOne-34").should('exist').click();
     cy.get("h2").contains("Info about adult");
     cy.get("#34").contains("34");
